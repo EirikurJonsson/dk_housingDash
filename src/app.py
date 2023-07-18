f042c0f07ef7747daf698349e807794fff9e274d
eirikur <eirikur@DESKTOP-LIM8OHC>
2023-07-19 00:50:02 +0200

    init commit

---
+++ src/app.py
@@ -0,0 +1,11 @@
+from dash import Dash
+import pandas as pd
+import json
+
+dummypath = './assets/dummydata/response.json'
+
+f = open(dummypath)
+data = json.load(f)
+
+df = pd.DataFrame.from_dict(data['results'])
+print(df)
---
+++ src/utils/getHousingData.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "https://api.boliga.dk/api/v2/search/results?pageSize=50&sort=daysForSale-a&propertyType=1,2,3,4,6&zipCodes=5210&includeds=1"
+
+payload = {}
+headers = {
+  'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+  'Accept': 'application/json, text/plain, */*',
+  'Referer': 'https://www.boliga.dk/',
+  'sec-ch-ua-mobile': '?0',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+  'sec-ch-ua-platform': '"Windows"'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.json())
