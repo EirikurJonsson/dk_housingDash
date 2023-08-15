482d1b04293beb2276cce537b77d9bbc602e3a8b
eirikur <eirikur@DESKTOP-LIM8OHC>
2023-08-15 22:29:38 +0200

    adding all changes and data

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ import pandas as pd
 import json
 from utils.getHousingData import getHousingData
 from components import createHousingTable
+import dash_leaflet as dl
+import dash_leaflet.express as dlx
 
 dummypath = './assets/dummydata/response.json'
 
@@ -11,20 +13,28 @@ f = open(dummypath)
 data = json.load(f)
 
 df = pd.DataFrame.from_dict(data['results'])
+geo = dlx.dicts_to_geojson('./geodata/dkPostnumbers.geojson')
+geo_json = dl.GeoJSON(data = geo)
 
-test = getHousingData(asPandas = True, postCode = 5210)
-print(len(test))
-print(len(test.drop_duplicates(subset = ['guid'])))
-
+#test = getHousingData(asPandas = True, postCode = 5210)
 
 app = Dash(
     __name__, 
     external_stylesheets=[dbc.themes.SLATE]
     )
 
-app.layout = dbc.Container(
-    createHousingTable(df = getHousingData(asPandas = True, postCode = 5210).sort_values('street'))
-)
+app.layout = dbc.html(
+    [
+        dbc.Container(
+            createHousingTable(df)
+    ),
+    dl.Map(
+        [
+            dl.TileLayer(), geo_json
+        ]
+    )
+    ]
+) 
 
 if __name__ == '__main__':
     app.run(debug = True, port = 8051)
